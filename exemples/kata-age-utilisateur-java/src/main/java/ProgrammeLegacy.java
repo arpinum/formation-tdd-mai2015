2e7349576e9e778cd8191e7015295de0ff183c70
Michael Borde <michael.borde@ubimail.fr>
2015-05-12 08:42:29 +0200

    Ajout exerce age utilisateur

--- exemples/kata-age-utilisateur-java/src/main/java/ProgrammeLegacy.java
+++ exemples/kata-age-utilisateur-java/src/main/java/ProgrammeLegacy.java
@@ -6,21 +6,11 @@ import java.util.Scanner;
 public class ProgrammeLegacy {
     public static void main(String[] args) {
         System.out.println("Date de naissance (ex 15/01/1983) ?");
-        String naissanceSaisie = récupèreDateNaissanceEnChaîne();
-        LocalDate dateNaissance = dateNaissance(naissanceSaisie);
-        long age = calculeAge(dateNaissance);
+        String naissanceSaisie = new Scanner(System.in, "UTF-8").nextLine();
+        LocalDate dateNaissance = LocalDate.parse(
+                naissanceSaisie,
+                DateTimeFormatter.ofPattern("dd/MM/yyyy"));
+        long age = dateNaissance.until(LocalDate.now(), ChronoUnit.YEARS);
         System.out.println(String.format("Vous avez %s ans", age));
-    }
-
-    private static String récupèreDateNaissanceEnChaîne() {
-        return new Scanner(System.in, "UTF-8").nextLine();
-    }
-
-    private static LocalDate dateNaissance(String naissanceSaisie) {
-        return LocalDate.parse(naissanceSaisie, DateTimeFormatter.ofPattern("dd/MM/yyyy"));
-    }
-
-    private static long calculeAge(LocalDate dateNaissance) {
-        return dateNaissance.until(LocalDate.now(), ChronoUnit.YEARS);
     }
 }
