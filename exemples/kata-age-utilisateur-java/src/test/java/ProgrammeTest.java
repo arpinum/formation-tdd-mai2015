458f386f92792f32d82c08ea3c29c82a94c85871
Michael Borde <michael.borde@ubimail.fr>
2015-05-12 23:20:02 +0200

    Ajout des tests mockito pour kata age utilisateur

--- exemples/kata-age-utilisateur-java/src/test/java/ProgrammeTest.java
+++ exemples/kata-age-utilisateur-java/src/test/java/ProgrammeTest.java
@@ -2,6 +2,7 @@ import es.EntréeStandard;
 import mocks.MockSortieStandard;
 import mocks.StubEntréeStandard;
 import mocks.StubTemps;
+import org.junit.Before;
 import org.junit.Test;
 import outils.Temps;
 
@@ -11,11 +12,15 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class ProgrammeTest {
 
+    @Before
+    public void before() throws Exception {
+        temps = new StubTemps(LocalDate.of(2015, 5, 11));
+    }
+
     @Test
     public void doitDemanderLaDateDeNaissanceDeLUtilisateurPourAfficherSonAge() {
         EntréeStandard entréeStandard = new StubEntréeStandard("27/04/1983");
         MockSortieStandard sortieStandard = new MockSortieStandard();
-        Temps temps = new StubTemps(LocalDate.of(2015, 5, 11));
         Programme programme = new Programme(entréeStandard, sortieStandard, temps);
 
         programme.exécuteToi();
@@ -23,4 +28,5 @@ public class ProgrammeTest {
         assertThat(sortieStandard.affichages).contains("Vous avez 32 ans");
     }
 
+    private Temps temps;
 }
